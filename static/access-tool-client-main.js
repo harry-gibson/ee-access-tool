b9d26fda8a24a0115229ed6d05a5a855d02bc953
Harry Gibson <harry.s.gibson@gmail.com>
2020-04-22 16:09:43 +0100

    client-side api update and fixes for undocumented EE-api changes, basic mapping working again

--- static/access-tool-client-main.js
+++ static/access-tool-client-main.js
@@ -146,10 +146,10 @@ access_tool.App.prototype.createMap = function(mapLayer){
   };
   var mapElement = $('.map').get(0);
   var map = new google.maps.Map(mapElement, mapOptions);
-  map.setOptions({styles: access_tool.App.SILVER_STYLES});
+  //map.setOptions({styles: access_tool.App.SILVER_STYLES});
   if (mapLayer) {
       // an initial overlay map such as the friction surface or a population map or something
-      map.overlayMapTypes.push(mapLayer);
+      //map.overlayMapTypes.push(mapLayer);
   }
   return map;
 };
@@ -695,7 +695,30 @@ access_tool.App.prototype.getFilename = function(){
     }
 };
 
-access_tool.App.prototype.getEeMapLayer_Tracked = function(eeMapId, eeToken) {
+access_tool.App.prototype.getEeMapLayer_Tracked = function(eeMapId){
+    if (eeMapId === "None") {
+        return null;
+    }
+    // on the python side, the map id is just a string.
+    // The EarthEngineTileSource expects (well, ee.Data.getTileUrl does) it to be an
+    // object with a property called formatTileUrl. It looks (line 20591) like it is intended
+    // to create this if it isn't present but it only checks (if id.!formatTileUrl), not
+    // if (id.formatTileUrl === undefined)
+    // There's an undocumented function below it called makeMapId:
+    var mapIdObj = ee.data.makeMapId_(eeMapId);
+    // The ImageOverlay differs from MapOverlay in that it needs an EarthEngineTileSource object,
+    // it seems (not documented, like every other fucking thing)
+    var tileSource = new ee.layers.EarthEngineTileSource(mapIdObj);
+    var layer = new ee.layers.ImageOverlay(tileSource);
+    layer.addTileCallback((function(event){
+        // the event's property for number tiles remaining has changed from count to loadingTileCount,
+        // not documented, like every other fucking thing
+        this.updateLoadStatus(event.loadingTileCount);
+    }).bind(this));
+    return layer;
+};
+
+access_tool.App.prototype.getEeMapLayer_Tracked_old = function(eeMapId, eeToken) {
   // return null if no ee overlay map is required (will then just make a plain googlemap)
   if (eeMapId === "None" || eeToken === "None"){
     return null;
@@ -706,7 +729,8 @@ access_tool.App.prototype.getEeMapLayer_Tracked = function(eeMapId, eeToken) {
       eeMapId, eeToken, {name: "Custom Accessibility Map"}
   );
   layer.addTileCallback((function(event){
-      this.updateLoadStatus(event.count);
+      this.updateLoadStatus(event.loadingTileCount);
+
   }).bind(this));
 
 
